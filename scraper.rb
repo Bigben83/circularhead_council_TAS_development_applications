2c969a8a10eae0e8f9be1194c51dcadf5c08fa6b
Benjamin Harris <Bigben83@users.noreply.github.com>
2025-02-06 14:20:12 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -8,7 +8,7 @@ require 'date'
 logger = Logger.new(STDOUT)
 
 # Define the URL of the page
-url = 'https://tasman.tas.gov.au/advertised-applications/'
+url = 'https://www.circularhead.tas.gov.au/council-services/development/planning/'
 
 # Step 1: Fetch the page content
 begin
